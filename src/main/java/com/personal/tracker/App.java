d9b16b931ba903fa0d647c079f4d3fcfdfd35ed2
zliel <zpliel@gmail.com>
2021-01-20 00:47:14 -0800

    Added tabs to separate the student and chapter tables

--- src/main/java/com/personal/tracker/App.java
+++ src/main/java/com/personal/tracker/App.java
@@ -1,5 +1,6 @@
 package com.personal.tracker;
 
+import com.personal.tracker.models.Chapter;
 import com.personal.tracker.models.Student;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
@@ -10,31 +11,31 @@ import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import java.util.ArrayList;
 
 public class App extends Application {
 
   @Override
   public void start(Stage stage) {
-    // Create a TableView for our database output
-    TableView<Student> databaseTable = new TableView<Student>();
-    // TODO - Look into getting data from the database into the table
+    TableView studentTable = createStudentTable();
+    TableView chapterTable = createChapterTable();
 
-    // Create an observable list for reactivity later
-    ObservableList<Student> list = FXCollections.observableArrayList(new Student("Jimmy", "John"));
-    databaseTable.setItems(list);
+    /* TODO TESTING */
+    // Create a tabPane to choose which part of the database to view
+    TabPane tabs = new TabPane();
 
-    // Set the columns' titles and data (the data comes from the Student.java file in the
-    // com.personal.tracker.models package
-    TableColumn<Student, String> firstNameCol = new TableColumn<>("First Name");
-    firstNameCol.setCellValueFactory(new PropertyValueFactory("firstName"));
-    TableColumn<Student, String> lastNameCol = new TableColumn<>("Last Name");
-    lastNameCol.setCellValueFactory(new PropertyValueFactory("lastName"));
+    // Create tabs
+    Tab studentTab = new Tab("Students", studentTable);
+    Tab chapterTab = new Tab("Chapters", chapterTable);
+    Tab testTab = new Tab("Testing!", new Label("This is a test tab! :)"));
 
-    // Set the data in the table
-    databaseTable.getColumns().setAll(firstNameCol, lastNameCol);
+    // Add the tabs to the TabPane
+    tabs.getTabs().add(studentTab);
+    tabs.getTabs().add(chapterTab);
+    tabs.getTabs().add(testTab);
 
     // Create our scene
-    Scene scene = new Scene(new StackPane(databaseTable), 1440, 900);
+    Scene scene = new Scene(new StackPane(tabs), 1440, 900);
     scene.setFill(Color.BLUEVIOLET);
 
     // All the stuff for the actual stage
@@ -52,6 +53,60 @@ public class App extends Application {
   public void stop() {
     // This should include anything to be done at the end of the program's lifecycle
     System.out.println("We did it");
+  }
+
+  public static TableView createStudentTable() {
+    // Move what's in start() here
+    // Create a TableView for our database output
+    TableView<Student> studentTable = new TableView<Student>();
+    // TODO - Look into getting data from the database into the table
+
+    // Get an ArrayList with students from the database
+    ArrayList<Student> students = Query.listStudents();
+
+    // Create an observable list for reactivity later
+    ObservableList<Student> list = FXCollections.observableArrayList(students);
+    studentTable.setItems(list);
+
+    // Set the columns' titles and data (the data comes from the Student.java file in the
+    // com.personal.tracker.models package
+    TableColumn<Student, Long> idCol = new TableColumn<>("ID");
+    idCol.setCellValueFactory(new PropertyValueFactory("id"));
+    TableColumn<Student, String> firstNameCol = new TableColumn<>("First Name");
+    firstNameCol.setCellValueFactory(new PropertyValueFactory("firstName"));
+    TableColumn<Student, String> lastNameCol = new TableColumn<>("Last Name");
+    lastNameCol.setCellValueFactory(new PropertyValueFactory("lastName"));
+
+    // Set the data in the table
+    studentTable.getColumns().setAll(idCol, firstNameCol, lastNameCol);
+    return studentTable;
+  }
+
+  public static TableView createChapterTable() {
+    // Create a TableView for our database output
+    TableView<Chapter> chapterTable = new TableView<>();
+    // TODO - Look into getting data from the database into the table
+
+    // Get an ArrayList with students from the database
+    ArrayList<Chapter> students = Query.listChapters();
+
+    // Create an observable list for reactivity later
+    ObservableList<Chapter> list = FXCollections.observableArrayList(students);
+    chapterTable.setItems(list);
+
+    // Set the columns' titles and data (the data comes from the Student.java file in the
+    // com.personal.tracker.models package
+
+    TableColumn<Chapter, Long> chapterNumCol = new TableColumn<>("Chapter Number");
+    chapterNumCol.setCellValueFactory(new PropertyValueFactory("chapterKey"));
+    TableColumn<Chapter, String> chapterTitleCol = new TableColumn<>("Chapter Title");
+    chapterTitleCol.setCellValueFactory(new PropertyValueFactory("chapterTitle"));
+    TableColumn<Chapter, String> bookTitleCol = new TableColumn<>("Book Title");
+    bookTitleCol.setCellValueFactory(new PropertyValueFactory("bookTitle"));
+
+    // Set the data in the table
+    chapterTable.getColumns().setAll(chapterNumCol, chapterTitleCol, bookTitleCol);
+    return chapterTable;
   }
 
 }
