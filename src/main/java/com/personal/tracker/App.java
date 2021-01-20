df25b850b5e7e317c3c08f51edfd64cddabba63a
zliel <zpliel@gmail.com>
2021-01-20 02:31:53 -0800

    Added a Completed Chapters tab

--- src/main/java/com/personal/tracker/App.java
+++ src/main/java/com/personal/tracker/App.java
@@ -1,6 +1,7 @@
 package com.personal.tracker;
 
 import com.personal.tracker.models.Chapter;
+import com.personal.tracker.models.CompletedChapter;
 import com.personal.tracker.models.Student;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
@@ -12,26 +13,30 @@ import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class App extends Application {
+  /* TODO - Add inputs to each tab for adding new data to the table */
 
   @Override
   public void start(Stage stage) {
-    TableView studentTable = createStudentTable();
-    TableView chapterTable = createChapterTable();
+    TableView<Student> studentTable = createStudentTable();
+    TableView<Chapter> chapterTable = createChapterTable();
+    TableView<CompletedChapter> completedChaptersTable = createCompletedChaptersTable();
 
-    /* TODO TESTING */
     // Create a tabPane to choose which part of the database to view
     TabPane tabs = new TabPane();
 
     // Create tabs
     Tab studentTab = new Tab("Students", studentTable);
     Tab chapterTab = new Tab("Chapters", chapterTable);
+    Tab completedChaptersTab = new Tab("Completed Chapters", completedChaptersTable);
     Tab testTab = new Tab("Testing!", new Label("This is a test tab! :)"));
 
     // Add the tabs to the TabPane
     tabs.getTabs().add(studentTab);
     tabs.getTabs().add(chapterTab);
+    tabs.getTabs().add(completedChaptersTab);
     tabs.getTabs().add(testTab);
 
     // Create our scene
@@ -49,13 +54,7 @@ public class App extends Application {
     launch();
   }
 
-  @Override
-  public void stop() {
-    // This should include anything to be done at the end of the program's lifecycle
-    System.out.println("We did it");
-  }
-
-  public static TableView createStudentTable() {
+  public static TableView<Student> createStudentTable() {
     // Move what's in start() here
     // Create a TableView for our database output
     TableView<Student> studentTable = new TableView<Student>();
@@ -71,18 +70,18 @@ public class App extends Application {
     // Set the columns' titles and data (the data comes from the Student.java file in the
     // com.personal.tracker.models package
     TableColumn<Student, Long> idCol = new TableColumn<>("ID");
-    idCol.setCellValueFactory(new PropertyValueFactory("id"));
+    idCol.setCellValueFactory(new PropertyValueFactory<>("id"));
     TableColumn<Student, String> firstNameCol = new TableColumn<>("First Name");
-    firstNameCol.setCellValueFactory(new PropertyValueFactory("firstName"));
+    firstNameCol.setCellValueFactory(new PropertyValueFactory<>("firstName"));
     TableColumn<Student, String> lastNameCol = new TableColumn<>("Last Name");
-    lastNameCol.setCellValueFactory(new PropertyValueFactory("lastName"));
+    lastNameCol.setCellValueFactory(new PropertyValueFactory<>("lastName"));
 
     // Set the data in the table
     studentTable.getColumns().setAll(idCol, firstNameCol, lastNameCol);
     return studentTable;
   }
 
-  public static TableView createChapterTable() {
+  public static TableView<Chapter> createChapterTable() {
     // Create a TableView for our database output
     TableView<Chapter> chapterTable = new TableView<>();
     // TODO - Look into getting data from the database into the table
@@ -98,15 +97,50 @@ public class App extends Application {
     // com.personal.tracker.models package
 
     TableColumn<Chapter, Long> chapterNumCol = new TableColumn<>("Chapter Number");
-    chapterNumCol.setCellValueFactory(new PropertyValueFactory("chapterKey"));
+    chapterNumCol.setCellValueFactory(new PropertyValueFactory<>("chapterKey"));
     TableColumn<Chapter, String> chapterTitleCol = new TableColumn<>("Chapter Title");
-    chapterTitleCol.setCellValueFactory(new PropertyValueFactory("chapterTitle"));
+    chapterTitleCol.setCellValueFactory(new PropertyValueFactory<>("chapterTitle"));
     TableColumn<Chapter, String> bookTitleCol = new TableColumn<>("Book Title");
-    bookTitleCol.setCellValueFactory(new PropertyValueFactory("bookTitle"));
+    bookTitleCol.setCellValueFactory(new PropertyValueFactory<>("bookTitle"));
 
     // Set the data in the table
     chapterTable.getColumns().setAll(chapterNumCol, chapterTitleCol, bookTitleCol);
     return chapterTable;
   }
 
+  public static TableView<CompletedChapter> createCompletedChaptersTable() {
+    // Create a TableView for our database output
+    TableView<CompletedChapter> completedChaptersTable = new TableView<>();
+    // TODO - Look into getting data from the database into the table
+
+    // Get an ArrayList with students from the database
+    ArrayList<CompletedChapter> completedChapters = Query.listCompletedChapters();
+
+    // Create an observable list for reactivity later
+    ObservableList<CompletedChapter> list = FXCollections.observableArrayList(completedChapters);
+    completedChaptersTable.setItems(list);
+
+    // Set the columns' titles and data (the data comes from the Student.java file in the
+    // com.personal.tracker.models package
+    TableColumn<CompletedChapter, Long> studentIdCol = new TableColumn<>("Student ID");
+    studentIdCol.setCellValueFactory(new PropertyValueFactory<>("studentId"));
+    TableColumn<CompletedChapter, Long> chapterNumCol = new TableColumn<>("Chapter Number");
+    chapterNumCol.setCellValueFactory(new PropertyValueFactory<>("chapterNumber"));
+    TableColumn<CompletedChapter, Date> completedDateCol = new TableColumn<>("Completion Date");
+    completedDateCol.setCellValueFactory(new PropertyValueFactory<>("completionDate"));
+    TableColumn<CompletedChapter, String> chapterTitleCol = new TableColumn<>("Chapter Title");
+    chapterTitleCol.setCellValueFactory(new PropertyValueFactory<>("chapterTitle"));
+    TableColumn<CompletedChapter, String> bookTitleCol = new TableColumn<>("Book Title");
+    bookTitleCol.setCellValueFactory(new PropertyValueFactory<>("bookTitle"));
+
+    // Set the data in the table
+    completedChaptersTable.getColumns().setAll(studentIdCol, chapterNumCol, chapterTitleCol, bookTitleCol);
+    return completedChaptersTable;
+  }
+
+  @Override
+  public void stop() {
+    // This should include anything to be done at the end of the program's lifecycle
+    System.out.println("We did it");
+  }
 }
