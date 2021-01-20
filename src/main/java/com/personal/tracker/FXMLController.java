3986609ad54202aed9ae7f3af81ae89fbd528e33
zliel <zpliel@gmail.com>
2021-01-19 22:11:44 -0800

    Added JavaFX to the project

---
+++ src/main/java/com/personal/tracker/FXMLController.java
@@ -0,0 +1,16 @@
+package com.personal.tracker;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+
+public class FXMLController implements Initializable {
+
+  @Override
+  public void initialize(URL url, ResourceBundle rb) {
+
+  }
+}
