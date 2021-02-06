29004273a0d7eb592150c7f419682f76279de222
zliel <zpliel@gmail.com>
2021-02-05 21:03:34 -0800

    Added Create and Delete functionality to the Student Tab

--- src/main/java/com/personal/tracker/views/StudentTab.java
+++ src/main/java/com/personal/tracker/views/StudentTab.java
@@ -1,5 +1,7 @@
 package com.personal.tracker.views;
 
+import com.personal.tracker.controller.Add;
+import com.personal.tracker.controller.Delete;
 import com.personal.tracker.models.Student;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -10,16 +12,18 @@ import javafx.scene.layout.VBox;
 
 public class StudentTab {
   public static Tab createStudentTab(TableView<Student> students) {
-    //Make text fields for each tab
+    //Make input fields for each necessary piece of information for the database
     TextField studentFirstNameField = new TextField();
     studentFirstNameField.setPromptText("Student First Name");
+    studentFirstNameField.setTooltip(new Tooltip("Student First Name"));
 
     TextField studentLastNameField = new TextField();
     studentLastNameField.setPromptText("Student Last Name");
+    studentLastNameField.setTooltip(new Tooltip("Student Last Name"));
 
-    // Labels for testing action handlers on buttons
-    Label firstNameLabel = new Label();
-    Label lastNameLabel = new Label();
+    // Labels for testing action handlers on buttons for testing (uncomment these in testing)
+//    Label firstNameLabel = new Label();
+//    Label lastNameLabel = new Label();
 
     // Creating our submit button and giving it a style rule
     Button submitButton = new Button("Submit");
@@ -29,10 +33,31 @@ public class StudentTab {
     submitButton.setOnAction(new EventHandler<>() {
       @Override
       public void handle(ActionEvent event) {
-        // Eventually this will add a new row to the database
-        firstNameLabel.setText("First Name: " + studentFirstNameField.getText());
-        lastNameLabel.setText("Last Name: " + studentLastNameField.getText());
+        String newFirstName = studentFirstNameField.getText();
+        String newLastName = studentLastNameField.getText();
+
+        // These are the labels for testing
+//        firstNameLabel.setText("First Name: " + studentFirstNameField.getText());
+//        lastNameLabel.setText("Last Name: " + studentLastNameField.getText());
+
+        Add.addStudent(newFirstName, newLastName);
+        Long studentId = Delete.getStudentId(newFirstName, newLastName);
+        students.getItems().add(new Student(studentId, newFirstName, newLastName));
       }
+    });
+
+    // Creating a button to let the user delete things from the database
+    Button deleteButton = new Button("Delete");
+    deleteButton.getStyleClass().add("delete-button");
+
+    // Adding delete functionality to our deleteButton
+    deleteButton.setOnAction(e -> {
+      Student selectedStudent = students.getSelectionModel().getSelectedItem();
+      students.getItems().remove(selectedStudent);
+
+      Long selectedStudentId = Delete.getStudentId(selectedStudent.getFirstName(),
+          selectedStudent.getLastName());
+      Delete.deleteStudent(selectedStudentId);
     });
 
     // Create a VBox to store the Tab's contents
@@ -54,12 +79,13 @@ public class StudentTab {
     inputGrid.setHgap(5);
     inputGrid.setVgap(5);
 
-    // Add contents to our GridPane
+    // Add contents to our GridPane (uncomment the labels when testing)
     inputGrid.add(studentFirstNameField, 0, 0);
-    inputGrid.add(firstNameLabel, 0, 1);
+    //inputGrid.add(firstNameLabel, 0, 1);
     inputGrid.add(studentLastNameField, 1, 0);
-    inputGrid.add(lastNameLabel, 1, 1);
+    //inputGrid.add(lastNameLabel, 1, 1);
     inputGrid.add(submitButton, 2, 0);
+    inputGrid.add(deleteButton, 3, 0);
 
     // Add the GridPane to our Vbox
     vbox.getChildren().addAll(students, inputGrid);
