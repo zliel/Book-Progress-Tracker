2a191e1823e8bd5afa706f67a8cd073156138a75
zliel <zpliel@gmail.com>
2021-04-06 23:09:16 -0700

    Updated the Student model class to generate ID values with the @GeneratedValue annotation

--- src/main/java/com/personal/tracker/models/Student.java
+++ src/main/java/com/personal/tracker/models/Student.java
@@ -1,17 +1,19 @@
 package com.personal.tracker.models;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Student {
   @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
   private Long id;
   private String firstName;
   private String lastName;
 
-  public Student(Long studentId, String firstName, String lastName) {
-    this.id = studentId;
+  public Student(String firstName, String lastName) {
     this.firstName = firstName;
     this.lastName = lastName;
   }
